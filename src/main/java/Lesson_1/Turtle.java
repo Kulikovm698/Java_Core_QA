d804fdff92ce4e393b82299e5daf7269b908dc8e
Maxim <kulikovm698@gmail.com>
2021-07-21 18:05:50 +0300

    Lesson_1(continuation)

---
+++ src/main/java/Lesson_1/Turtle.java
@@ -0,0 +1,8 @@
+package Lesson_1;
+
+public class Turtle extends Animal {
+
+    public Turtle(String name, String color, int age) {
+        super(name, color, age);
+    }
+}
---
+++ src/main/java/Lesson_1/Wolf.java
@@ -0,0 +1,8 @@
+package Lesson_1;
+
+public class Wolf extends Animal {
+
+    public Wolf(String name, String color, int age) {
+        super(name, color, age);
+    }
+}
