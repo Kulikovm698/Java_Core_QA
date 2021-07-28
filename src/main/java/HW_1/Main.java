24dedcc5437fe56bd2102d5e24f63d9deec0fd7b
Maxim <kulikovm698@gmail.com>
2021-07-28 18:23:24 +0300

    HW_1. Урок 1. Класс Object

---
+++ src/main/java/HW_1/Main.java
@@ -0,0 +1,21 @@
+package HW_1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Competing[] competings = {new Cat("Кот Барсик", 5, 10),
+                new Human("Борис", 1, 20),
+                new Robot("Робот Валли", 4, 40)};
+    }
+
+    Overcomeble[] overcomebles = {new Wall(1), new RoadRun(2)};
+
+
+    for(Competing competing : competings){ //никак не могу понять в чем ошибка в этой строчке???
+        for(Overcomeble overcomeble : overcomebles){
+            overcomeble.overcome(competing);
+        }
+    }
+
+    }
+
